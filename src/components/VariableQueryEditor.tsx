9f2626c5f66c81f21a60aaf6fcdec5a6ec792f33
Rico Berger <ricoberger@users.noreply.github.com>
2026-02-24 19:42:03 +0100

    Add Support for Variables
    The datasource can now be used in variables. Currently only zones are
    supported as variable types.

---
+++ src/components/VariableQueryEditor.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { QueryEditorProps } from '@grafana/data';
+import {
+  InlineFieldRow,
+  InlineField,
+  Combobox,
+  ComboboxOption,
+} from '@grafana/ui';
+
+import { DataSource } from '../datasource';
+import { DEFAULT_QUERIES, Options, Query, QueryType } from '../types';
+
+interface Props extends QueryEditorProps<DataSource, any, Options, Query> { }
+
+export function VariableQueryEditor({ query, onChange, onRunQuery }: Props) {
+  return (
+    <>
+      <InlineFieldRow>
+        <InlineField label="Variable Type" labelWidth={25}>
+          <Combobox<QueryType>
+            value={query.queryType}
+            options={[
+              {
+                label: 'Zones',
+                value: 'zones',
+              },
+            ]}
+            onChange={(option: ComboboxOption<QueryType>) => {
+              onChange({
+                ...query,
+                ...DEFAULT_QUERIES[option.value],
+                queryType: option.value,
+              });
+              onRunQuery();
+            }}
+          />
+        </InlineField>
+      </InlineFieldRow>
+    </>
+  );
+}
